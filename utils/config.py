4c4464e77aebcf48b81356b7e27075a00edfaab8
Jhon Alejandro Moncaleano Forero <98182010+JhonaleMF@users.noreply.github.com>
2022-12-01 11:07:15 +0100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-username = "admin"
-password = "12345678"
+username = "XXXXXX"
+password = "XXXXXXXX"
 host = "aseafdatabase.cetjlbc4c4nw.us-east-1.rds.amazonaws.com" #  "database-2.crerrxwepn0b.us-east-2.rds.amazonaws.com"
 port = 3306
